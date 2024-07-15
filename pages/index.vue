597c1b3e639ab33c98083f49d8a73c512f2db7af
digkill <digkill@mail.ru>
2024-07-15 12:35:24 +0500

    Added pages

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <section class="flex flex-col items-center justify-center min-h-screen px-6 lg:px-8">
+    <h1 class="font-bold text-2xl leading-tight mb-10">
+      CRM System by MEDIARISE
+    </h1>
+  </section>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  useHead({
+    title: 'Login'
+  })
+</script>
+<template>
+  <section class="flex items-center justify-center min-h-screen w-full">
+    <div class="rounded bg-sidebar w-1/4 p-5">
+      <h1 class="text-2x1 font-bold text-center mb-5">Sign In</h1>
+
+      <form>
+
+        <UIInput placeholder="Email" type="email" class="mb-3"/>
+      </form>
+    </div>
+  </section>
+</template>
